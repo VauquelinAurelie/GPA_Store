9dd42109d1735413d9dea1bb8aef0262dd31ab40
vauquelinAurelie <aurelie.vauquelin@le-campus-numerique.fr>
2024-07-29 11:50:11 +0200

    categories page / display products from all categories

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,5 +30,35 @@ class ProductController extends Controller
             return response()->json(['error' => 'An error occurred while fetching products'], 500);
         }
     }
+
+    // categories page - display products from all categories
+    public function categories()
+    {
+        $categories = ['electronics', 'jewelery', 'men\'s clothing', 'women\'s clothing'];
+
+        $categoryProducts = [];
+        foreach ($categories as $category) {
+            $encodedCategory = rawurlencode($category);
+            $url = "https://fakestoreapi.com/products/category/{$encodedCategory}";
+
+            Log::info("Fetching products for category '{$category}' from URL: {$url}");
+
+            try {
+                $response = Http::get($url);
+
+                if ($response->successful()) {
+                    $categoryProducts[$category] = $response->json();
+                } else {
+                    Log::error("API response error for category '{$category}': " . $response->status());
+                    $categoryProducts[$category] = [];
+                }
+            } catch (\Exception $e) {
+                Log::error("Exception fetching products for category '{$category}': " . $e->getMessage());
+                $categoryProducts[$category] = [];
+            }
+        }
+
+        return view('categories.index', ['categoryProducts' => $categoryProducts]);
+    }
 }
 
